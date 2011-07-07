2f15ebb9574821f76ca1fb95754b8b9e24d13c58
Lin Ouyang <lin.s.ouyang@gmail.com>
2011-07-06 23:53:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ETwoOne::Application.config.secret_token = '6abc45c2d3ef564f90db1e5b412bf1c4754810eca1b8dfaf835b30e684ac025a2beeb65214ca6854ef30e224118547f7c452642d40a045a22bd95dd343482db7'
